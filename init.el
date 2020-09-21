410c8a6c4e70249141bff74ef6bfd8bc452758f3
JackLovel <gogkatsu@outlook.com>
2020-09-21 13:28:30 +0800

    fix

--- init.el
+++ init.el
@@ -20,12 +20,19 @@
   :ensure t
   :config (which-key-mode))
 
+
+;; org-mode stuff
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda() (org-bullets-mode 1))))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(which-key try use-package)))
+ '(package-selected-packages '(org-bullets which-key try use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
