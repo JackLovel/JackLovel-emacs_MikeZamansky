6e156b1ee64b2bbd3511cd1d019666ef7c2791d9
JackLovel <gogkatsu@outlook.com>
2020-09-21 20:56:02 +0800

    使用 org 来管理配置

--- init.el
+++ init.el
@@ -1,7 +1,5 @@
 ;; window 系统：%HOMEPATH%\AppData\Roaming\.emacs.d
 ;; linux 系统：~/.emacs.d
-(setq inhibit-startup-message t)
-(tool-bar-mode -1)
 
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -13,89 +11,6 @@
 (unless (package-installed-p 'use-package)
 	(package-refresh-contents)
 	(package-install 'use-package))
-
-(use-package try
-  :ensure t)
-
-(use-package which-key
-  :ensure t
-  :config (which-key-mode))
-
-
-;; org-mode stuff
-(use-package org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda() (org-bullets-mode 1))))
-
-
-;; buffer switch: C-x b 
-(setq indo-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-;; buffer 列表
-(defalias 'list-buffers 'ibuffer)
-
-
-;; 窗口跳转
-;; M-o 1
-(use-package ace-window
-  :bind ("M-o" . ace-window)
-  :ensure t
-  :init
-  (progn
-   ;; (global-set-key [remap other-window] 'ace-window)
-    (custom-set-faces
-     '(aw-leading-char-face
-       ((t (:inherit ace-jump-face-foreground :height 3.0)))))
-    ))
-
-;; it looks like counsel is a requirement for swiper
-(use-package counsel
-  :ensure t)
-
-(use-package ivy 
-  :ensure t
-  :diminish (ivy-mode)
-  :bind (("C-x b" . ivy-switch-buffer))
-  :config
-  (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-display-style 'fancy))
-
-;; 搜索: swiper
-(use-package swiper
-  :ensure try
-  :bind (("C-s" . swiper)
-	 ("C-r" . swiper)
-	 ("C-c C-r" . ivy-resume)
-	 ("M-x" . counsel-M-x)
-	 ("C-x C-f" . counsel-find-file))
-  :config
-  (progn
-    (ivy-mode 1)
-    (setq ivy-use-virtual-buffers t)
-    (setq ivy-display-style 'fancy)
-    (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
-    ))
-
-(use-package avy
-  :ensure t
-  :bind ("M-s" . avy-goto-char))
-
-(use-package auto-complete
-  :ensure t
-  :init
-  (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)
-    ))
-
-(use-package zenburn-theme
-  :ensure t
-  :config (load-theme 'zenburn t)
-  )
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -110,4 +25,7 @@
  ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
 
-(org-babel-load-file (expand-file-name "myinit.org"))
+;; font-size: 16pt 
+(set-face-attribute 'default nil :height 170)
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
