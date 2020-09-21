f315752f0ae1a9807d7d227b88635d3ef8d5fb1d
JackLovel <gogkatsu@outlook.com>
2020-09-21 20:28:28 +0800

    org init

--- init.el
+++ init.el
@@ -102,10 +102,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zenburn-theme color-theme auto-complete counsel swiper ace-window tabbar org-bullets which-key try use-package)))
+   '(elfeed-org zenburn-theme color-theme auto-complete counsel swiper ace-window tabbar org-bullets which-key try use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+
+(org-babel-load-file (expand-file-name "myinit.org"))
