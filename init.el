32a256ee924222305950823e5b870a805e0c2070
JackLovel <gogkatsu@outlook.com>
2020-09-21 14:42:52 +0800

    buffers

--- init.el
+++ init.el
@@ -1,13 +1,14 @@
 ;; window 系统：%HOMEPATH%\AppData\Roaming\.emacs.d
 ;; linux 系统：~/.emacs.d
 (setq inhibit-startup-message t)
+(tool-bar-mode -1)
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/"))
 			
 (package-initialize)
-
 
 (unless (package-installed-p 'use-package)
 	(package-refresh-contents)
@@ -27,12 +28,26 @@
   :config
   (add-hook 'org-mode-hook (lambda() (org-bullets-mode 1))))
 
+;; buffer switch: C-x b 
+(setq indo-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+;; buffer 列表
+(defalias 'list-buffers 'ibuffer)
+;;(defalias 'list-buffers 'ibuffer-other-window)
+
+;; If you like a tabbar
+;(use-package tabbar
+;  :ensure t
+;  :config
+;  (tabbar-mode 1))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(org-bullets which-key try use-package)))
+ '(package-selected-packages '(tabbar org-bullets which-key try use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
