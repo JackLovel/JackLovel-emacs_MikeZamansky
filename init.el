7bab65a506f67622e4d6e49fa65f7c1c8ec182cd
JackLovel <gogkatsu@outlook.com>
2020-09-21 18:22:56 +0800

    auto complete

--- init.el
+++ init.el
@@ -83,13 +83,21 @@
   :ensure t
   :bind ("M-s" . avy-goto-char))
 
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)
+    ))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(counsel swiper ace-window tabbar org-bullets which-key try use-package)))
+   '(auto-complete counsel swiper ace-window tabbar org-bullets which-key try use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
