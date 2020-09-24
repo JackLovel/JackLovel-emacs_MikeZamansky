ab7e7dece45597b1b7c275aa1b71c53cc398ac14
JackLovel <gogkatsu@outlook.com>
2020-09-24 12:07:48 +0800

    shell and eshell

--- init.el
+++ init.el
@@ -16,8 +16,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-default-notes-file (concat org-directory "/notes.org"))
+ '(org-directory "~/Dropbox/orgfiles")
+ '(org-export-html-postamble nil)
+ '(org-hide-leading-stars t)
+ '(org-startup-folded (quote overview))
+ '(org-startup-indented t)
  '(package-selected-packages
-   '(web-mode miniedit iedit ledit expand-region hungry-delete beacon undo-tree elpy jedi flycheck ox-reveal elfeed-org zenburn-theme color-theme auto-complete counsel swiper ace-window tabbar org-bullets which-key try use-package)))
+   (quote
+    (virtualenvwrapper web-mode miniedit iedit ledit expand-region hungry-delete beacon undo-tree elpy jedi flycheck ox-reveal elfeed-org zenburn-theme color-theme auto-complete counsel swiper ace-window tabbar org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
