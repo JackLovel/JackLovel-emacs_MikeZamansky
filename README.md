ffa5efef259383dc173b79961b064b38511eb313
JackLovel <gogkatsu@outlook.com>
2020-09-25 10:56:17 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 ﻿# JackLovel-emacs_MikeZamansky
 
 [MikeZamansky emacs 教程](https://cestlaz.github.io/stories/emacs/)
+
+# deb package
+```
+// counsel-ag depend
+$ sudo apt install -y silversearcher-ag
+
+// ggtags depend or use speedbar  
+$ sudo apt install -y global
+```
--- elpa/ivy-20200826.955/ivy-pkg.el
+++ elpa/ivy-20200826.955/ivy-pkg.el
@@ -1,6 +1,6 @@
 (define-package "ivy" "20200826.955" "Incremental Vertical completYon"
   '((emacs "24.5"))
-  :commit "7e4c56776f811f78b8eb95210156f8fbbdba67e7" :keywords
+  :commit "b65e401c22ec56a008b00f651cd9536caf593d43" :keywords
   '("matching")
   :authors
   '(("Oleh Krehel" . "ohwoeowho@gmail.com"))
