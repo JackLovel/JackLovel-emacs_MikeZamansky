550bee373cab6296624c1e09913e47135abdc359
JackLovel <gogkatsu@outlook.com>
2020-09-25 11:21:08 +0800

    fix

--- elpa/better-shell-20191025.1737/better-shell-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from c:/Users/21017/AppData/Roaming/.emacs.d/elpa/better-shell-20191025.1737/better-shell.el  -*- no-byte-compile: t -*-
-(define-package "better-shell" "20191025.1737" "Better shell management" '((emacs "24.4")) :commit "70c787b981caeef8c5f8012b170eb7b9f167cd13" :keywords '("convenience") :authors '(("Russell Black" . "killdash9@github")) :maintainer '("Russell Black" . "killdash9@github") :url "https://github.com/killdash9/better-shell")
